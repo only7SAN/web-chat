27cd70076fdd3acd21518d23a87f4188f8aa23b0
only7SAN <1121428790@qq.com>
2017-05-13 23:31:11 +0800

    初始化结构

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-#  准备用vue 搭建一个实时聊天室
+# web-chat
+
+> 准备用 vue +  stocket.io 搭建一个实时聊天室
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
+
---
+++ src/components/Hello.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>web-chat聊天室</h1>
+    <h2>Essential Links</h2>
+  </div>
+</template>
+
+<script>
+  const socket = require('socket.io-client')('http://localhost:8080');
+
+  const say = () => {
+    console.log('aacc');
+  };
+  socket.on('connect', say);
+  socket.on('chat message', say);
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
